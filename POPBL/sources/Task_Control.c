8f00e15f41cc7e10b8561b247bfa8cc43a316015
Aitor <aitor.arrietam@alumni.mondragon.edu>
2013-05-23 17:49:24 +0200

    Introduccion codigo simulink

--- POPBL/sources/Task_Control.c
+++ POPBL/sources/Task_Control.c
@@ -3,14 +3,25 @@
 #include <stdio.h>
 #include "consolaLCD/console.h"
 
+#include "simulink_code/stellaris_1.h"                /* Model's header file */
+#include "simulink_code/rtwtypes.h"                  /* MathWorks types */
+
+double ref_motor1 = 0;
+double ref_motor2 = 0;
+
+void rt_OneStep(void);
+
 void vTask_Control( void *pvParameters ){
 	char str[32];
 	  int i,cnt=0;
 	  portTickType xLastWakeTime, period = (portTickType) pvParameters;
 
 	  xLastWakeTime=xTaskGetTickCount();
+
 	  while(1)
 	  {
+
+		  rt_OneStep();
 	    for(i=0;i<16000;i++);
 	    cnt++;
 
@@ -21,4 +32,48 @@ void vTask_Control( void *pvParameters ){
 
 	    vTaskDelayUntil(&xLastWakeTime, period/portTICK_RATE_MS);
   }
+}
+
+
+void rt_OneStep(void)
+{
+  static boolean_T OverrunFlag = 0;
+
+  /* Disable interrupts here */
+
+  /* Check for overrun */
+  if (OverrunFlag) {
+    rtmSetErrorStatus(stellaris_1_M, "Overrun");
+    return;
+  }
+
+  OverrunFlag = TRUE;
+
+  /* Save FPU context here (if necessary) */
+  /* Re-enable timer or interrupt here */
+  /* Set model inputs here */
+
+  stellaris_1_U.Sens1 = 0;
+  stellaris_1_U.Sens2 = 0;
+  stellaris_1_U.Sens3 = 0;
+  stellaris_1_U.Sens4 = 0;
+  stellaris_1_U.Sens5 = 0;
+  stellaris_1_U.Sens6 = 0;
+  stellaris_1_U.STOP = false;
+
+
+  /* Step the model */
+  stellaris_1_step();
+
+  /* Get model outputs here */
+
+  ref_motor1  = stellaris_1_Y.Ref_Motor1;
+  ref_motor2  = stellaris_1_Y.Ref_Motor2;
+
+  /* Indicate task complete */
+  OverrunFlag = FALSE;
+
+  /* Disable interrupts here */
+  /* Restore FPU context here (if necessary) */
+  /* Enable interrupts here */
 }
--- POPBL/sources/main.c
+++ POPBL/sources/main.c
@@ -22,9 +22,10 @@ int main( void )
 {
   SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ);
   initConsole();
+  stellaris_1_initialize(); //Stellaris
   consolePrintStr( 1, 1,"Hello POPBL");
   refreshConsoleLine(1);
-  xTaskCreate( vTask_Control,"vTask_Control",200, 1000 ,1, NULL );
+  xTaskCreate( vTask_Control,"vTask_Control",200, 50 ,1, NULL );
   xTaskCreate( vTask_Comunicaciones,"vTask_Comunicaciones",200, 2000 ,2, NULL );
   vTaskStartScheduler();
   for( ;; );
